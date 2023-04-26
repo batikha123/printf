f2a38fe49b6ddc20236b1c08b87c43a70a7f57e8
Youssef Abbar <abbaryoussef345@gmail.com>
2023-04-25 17:46:06 -0700

    Fixed betty issues

--- print_hex.c
+++ print_hex.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_hex - prints an unsigned int in hexidecimal form
  * @n: unsigned int to print
@@ -30,7 +29,6 @@ int print_hex(unsigned int n, unsigned int c)
 		m /= 16;
 		a[i] = (n / m) % 16;
 	}
-
 	for (i = 0, sum = 0, count = 0; i < 8; i++)
 	{
 		sum += a[i];
@@ -46,29 +44,24 @@ int print_hex(unsigned int n, unsigned int c)
 			count++;
 		}
 	}
-
 	return (count);
 }
-
 /**
  * print_x - takes an unsigned int and prints it in lowercase hex notation
  * @x: unsigned int to print
  *
  * Return: number of digits printed
  */
-
 int print_x(va_list x)
 {
 	return (print_hex(va_arg(x, unsigned int), 0));
 }
-
 /**
  * print_X - takes am unsigned int and prints it in uppercase hex notation
  * @X: unsigned int to print
  *
  * Return: number of digits printed
  */
-
 int print_X(va_list X)
 {
 	return (print_hex(va_arg(X, unsigned int), 1));
@@ -81,7 +74,6 @@ int print_X(va_list X)
  *
  * Return: base ^ exponent
  */
-
 static unsigned long _pow(unsigned int base, unsigned int exponent)
 {
 	unsigned int i;
@@ -91,22 +83,18 @@ static unsigned long _pow(unsigned int base, unsigned int exponent)
 	{
 		ans *= base;
 	}
-
 	return (ans);
 }
-
 /**
  * print_p - prints an address
  * @p: address to print
  *
  * Return: number of characters to print
  */
-
 int print_p(va_list p)
 {
 	int count = 0;
-	unsigned int a[16];
-	unsigned int i, sum;
+	unsigned int a[16], i, sum;
 	unsigned long n, m;
 	char *str = "(nil)";
 
@@ -117,44 +105,32 @@ int print_p(va_list p)
 		for (i = 0; str[i]; i++)
 		{
 			_putchar(str[i]);
-
 			count++;
 		}
-
 		return (count);
 	}
+
 	_putchar('0');
-
 	_putchar('x');
-
 	count = 2;
-
 	m = _pow(16, 15); /* 16 ^ 15 */
 	a[0] = n / m;
-
 	for (i = 1; i < 16; i++)
 	{
 		m /= 16;
-
 		a[i] = (n / m) % 16;
 	}
-
 	for (i = 0, sum = 0; i < 16; i++)
 	{
 		sum += a[i];
-
 		if (sum || i == 15)
 		{
 			if (a[i] < 10)
-
 				_putchar('0' + a[i]);
-
 			else
 				_putchar('0' + ('a' - ':') + a[i]);
-
 			count++;
 		}
 	}
-
 	return (count);
 }
