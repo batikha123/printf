f0d630b3c8301e78cfe33d0a3b4a24b6a8718c21
Youssef Abbar <abbaryoussef345@gmail.com>
2023-04-25 17:36:20 -0700

    Fixing betty issues

--- print_numbers.c
+++ print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_i - prints an integer
  * @i: integer to print
@@ -10,32 +9,25 @@
 int print_i(va_list i)
 {
 	int a[10];
-
 	int j, m, n, sum, count;
 
 	n = va_arg(i, int);
-
 	count = 0;
-
 	m = 1000000000;
-
 	a[0] = n / m;
 
 	for (j = 1; j < 10; j++)
 	{
 		m /= 10;
-
 		a[j] = (n / m) % 10;
 	}
 
 	if (n < 0)
 	{
 		_putchar('-');
-
 		count++;
 
 		for (j = 0; j < 10; j++)
-
 			a[j] *= -1;
 	}
 
@@ -46,11 +38,9 @@ int print_i(va_list i)
 		if (sum != 0 || j == 9)
 		{
 			_putchar('0' + a[j]);
-
 			count++;
 		}
 	}
-
 	return (count);
 }
 
@@ -64,47 +54,37 @@ int print_i(va_list i)
 int print_d(va_list d)
 {
 	int a[10];
-
 	int j, m, n, sum, count;
 
 	n = va_arg(d, int);
-
 	count = 0;
-
 	m = 1000000000;
-
 	a[0] = n / m;
 
 	for (j = 1; j < 10; j++)
 	{
 		m /= 10;
-
 		a[j] = (n / m) % 10;
 	}
 
 	if (n < 0)
 	{
 		_putchar('-');
-
 		count++;
 
 		for (j = 0; j < 10; j++)
-
 			a[j] *= -1;
 	}
 
 	for (j = 0, sum = 0; j < 10; j++)
-
 	{
 		sum += a[j];
 
 		if (sum != 0 || j == 9)
 		{
 			_putchar('0' + a[j]);
-
 			count++;
 		}
 	}
-
 	return (count);
 }
